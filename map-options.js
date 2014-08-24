31f663c2d4bdae321fbeb8bb71b016b7999039f2
Tim Beckett <timbeckett@ymail.com>
2014-08-24 16:11:27 -0400

    End of lesson 3.7'dealing with multiple events'

--- map-options.js
+++ map-options.js
@@ -16,6 +16,21 @@
     },
     panControlOptions: {
       position: google.maps.ControlPosition.LEFT_BOTTOM
+    }, 
+    cluster: {
+      options: {
+        styles: [{
+          url: 'http://google-maps-utility-library-v3.googlecode.com/svn/trunk/markerclusterer/images/m2.png',
+          height: 56,
+          width: 55,
+          textColor: '#F00',
+          textSize: 18
+        },{
+          url: 'http://google-maps-utility-library-v3.googlecode.com/svn/trunk/markerclusterer/images/m1.png',
+          height: 56,
+          width: 55
+        }]
+      }
     }
   };
   
--- script.js
+++ script.js
@@ -21,14 +21,27 @@
   var marker2 = map.addMarker({
   	lat: 59.570325, 
   	lng: -108.615554, 
-  	draggable: true, 
+  	draggable: true,
+    events: [{
+      name: 'click', 
+      callback: function(e, marker) {
+        console.log(e, marker); 
+      }
+    }, {
+      name: 'dragged', 
+      callback: function() {
+        console.log(e, marker); 
+      }
+    }],  
   	visible: true,
   	id: 'marker-2',
   	content: "<div class='info-box'>You are in Uranium City</div>", 
   	icon: "http://uraniumcity-history.com/wp-content/themes/uc-history-video/images/atomic-symbol.png"
   }); 
+
+  console.log(marker2); 
   
-  map.zoom(14); 
+  //map.zoom(14); 
 
   /*Single Responsibility Principle: 
   A responsiblity is anything that has a tendency to change. 
@@ -43,15 +56,32 @@
 //   });
 // console.log(found);
 
-  map.removeBy(function(marker) {
-    return marker.id === 'marker-2'; 
-  }); 
+  // map.removeBy(function(marker) {
+  //   return marker.id === 'marker-2'; 
+  // }); 
 
   // var randomArr = [1,2,3]; 
 
   // randomArr.forEach(function(item) {
   //   alert(item);
   // }); 
+
+  // for( var i=0; i < 10; i++) {
+  //   map.addMarker({
+  //     id: marker-2, 
+  //     lat: 59.570325 + Math.random(), 
+  //     lng: -108.615554 + Math.random(),
+  //     content: 'This is Uranium City', 
+  //     icon: "http://uraniumcity-history.com/wp-content/themes/uc-history-video/images/atomic-symbol.png"
+  //   }); 
+
+  //   var marker = map.addMarker({
+  //     lat: 59.570325 + Math.random(), 
+  //     lng: -108.605554 + Math.random(),
+  //     content: 'This is outside Uranium City', 
+  //     icon: "http://uraniumcity-history.com/wp-content/themes/uc-history-video/images/atomic-symbol.png"
+  //   }); 
+  // }
  
   
 }(window, google, window.Mapster));
